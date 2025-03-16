922548d4e36198a870aa9a9ec305f71fb6405ec7
st3pp3r <samtepper2323@gmail.com>
2025-03-15 22:41:10 -0400

    message placeholder

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,7 +1,7 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `Ramses the Resume GOAT 🐏`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const MESSAGE_PLACEHOLDER: string = `Message for Ramses Here`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
 export const FOOTER_MESSAGE: string = `Resume Building AI Assistant by Stepper @ UNC (BETA VERSION Document Uploading coming soon)`;
 export const PAGE_TITLE: string = `RAMSES`;
