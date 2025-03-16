2a615c7686fd1b587ce92171c1b6234f5f6b685d
st3pp3r <samtepper2323@gmail.com>
2025-03-15 22:03:29 -0400

    Basic Page UI updates

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,9 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
+export const CHAT_HEADER: string = `Ramses the Resume GOAT 🐏`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `Customized AI by Stepper @ UNC`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const FOOTER_MESSAGE: string = `Resume Building AI Assistant by Stepper @ UNC (BETA VERSION Document Uploading coming soon)`;
+export const PAGE_TITLE: string = `RAMSES`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
