caab0b99d918f6375dc57abe2b32b22bbbf0c251
st3pp3r <samtepper2323@gmail.com>
2025-03-15 22:10:33 -0400

    Clear message Adjustment

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -2,6 +2,7 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `Ramses the Resume GOAT 🐏`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
 export const FOOTER_MESSAGE: string = `Resume Building AI Assistant by Stepper @ UNC (BETA VERSION Document Uploading coming soon)`;
 export const PAGE_TITLE: string = `RAMSES`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
